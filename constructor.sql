8516378f12b88b0b1f95b2b804d330a5e7c8d49a
Im2Slow <adrien.kerisit@gmail.com>
2018-03-13 14:04:56 +0100

    Added auto_increment to constructor script for int primary keys

--- constructor.sql
+++ constructor.sql
@@ -2,7 +2,7 @@ create database `escapade`;
 use escapade;
 
 create table `escapade`.`stay` (
-	`id` int not null,
+	`id` int not null auto_increment, 
 	`theme` varchar(3) not null,
 	primary key (`id`) );
 
@@ -18,7 +18,7 @@ create table `escapade`.`car` (
 	primary key (`id`) );
 
 create table `escapade`.`client` (
-	`id` int not null,
+	`id` int not null auto_increment,
 	`firstname` varchar(20) not null,
 	`lastname` varchar(20) not null,
 	`phone` varchar(12) not null,
@@ -27,13 +27,13 @@ create table `escapade`.`client` (
 	primary key (`id`) );
 
 create table `escapade`.`housing` ( 
-	`id` int not null,
+	`id` int not null auto_increment,
 	`name` varchar(10),
 	`theme` varchar(3) not null,
 	primary key (`id`) );
 
 create table `escapade`.`deal` (
-	`id` int not null,
+	`id` int not null auto_increment,
 	`id_stay` int not null,
 	`id_car` varchar(10) not null,
 	`id_client` int not null,
@@ -46,18 +46,18 @@ create table `escapade`.`deal` (
 	primary key (`id`) );
 
 create table `escapade`.`supervisor` (
-	`id` int not null,
+	`id` int not null auto_increment,
 	`firstname` varchar(10) not null,
 	`lastname` varchar(10) not null,
 	primary key (`id`) );
 
 create table `escapade`.`parking` (
-	`id` int not null,
+	`id` int not null auto_increment,
 	`theme` varchar(3) not null,
 	primary key (`id`) );
 
 create table `escapade`.`maintenance` (
-	`id` int not null,
+	`id` int not null auto_increment,
 	`id_car` varchar(10) not null,
 	`cause` varchar(100) not null,
 	`intervention` varchar(100),
